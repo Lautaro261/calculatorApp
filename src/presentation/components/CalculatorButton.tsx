b82d9926a400236c0fe26ee5fa326ab3351f5871
Nahuel Lautaro Torres Loretto <112157731+Lautaro261@users.noreply.github.com>
2024-08-30 19:58:41 -0300

    Tarea completada: maquetado

--- src/presentation/components/CalculatorButton.tsx
+++ src/presentation/components/CalculatorButton.tsx
@@ -1,28 +1,35 @@
 import React from 'react';
 import {Text, Pressable} from 'react-native';
-import { colors, globalStyles } from '../../config/theme/app-theme';
+import {colors, globalStyles} from '../../config/theme/app-theme';
 
 interface Props {
   label: string;
   color?: string;
-
+  doubleSize?: boolean;
+  blackText?: boolean;
 }
 
 export const CalculatorButton = ({
   label,
   color = colors.darkGray,
-}:Props) => {
-
-
+  doubleSize = false,
+  blackText = false,
+}: Props) => {
   return (
-    <Pressable style={({pressed})=>({
-      ...globalStyles.button,
-      backgroundColor: color,
-      opacity: (pressed) ? 0.8 : 1
-    })}>
-    <Text style={globalStyles.buttonText}>{label}</Text>
-  </Pressable>
+    <Pressable
+      style={({pressed}) => ({
+        ...globalStyles.button,
+        backgroundColor: color,
+        width: doubleSize ? 180 : 80,
+        opacity: pressed ? 0.8 : 1,
+      })}>
+      <Text
+        style={{
+          ...globalStyles.buttonText,
+          color: blackText ? 'black' : 'white',
+        }}>
+        {label}
+      </Text>
+    </Pressable>
   );
 };
-
-
--- src/presentation/screens/CalculatorScreen.tsx
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import {colors, globalStyles} from '../../config/theme/app-theme';
-import { CalculatorButton } from '../components/CalculatorButton';
+import {CalculatorButton} from '../components/CalculatorButton';
 
 export const CalculatorScreen = () => {
   return (
@@ -12,12 +12,33 @@ export const CalculatorScreen = () => {
       </View>
 
       <View style={globalStyles.row}>
-
-        <CalculatorButton label="C" color={colors.lightGray}/>
-        <CalculatorButton label="+/-" color={colors.lightGray}/>
-        <CalculatorButton label="del" color={colors.lightGray}/>
+        <CalculatorButton label="C" color={colors.lightGray} blackText={true}/>
+        <CalculatorButton label="+/-" color={colors.lightGray} blackText={true}/>
+        <CalculatorButton label="del" color={colors.lightGray} blackText={true}/>
         <CalculatorButton label="/" color={colors.orange} />
-
+      </View>
+      <View style={globalStyles.row}>
+        <CalculatorButton label="7"  />
+        <CalculatorButton label="8"  />
+        <CalculatorButton label="9"  />
+        <CalculatorButton label="x" color={colors.orange} />
+      </View>
+      <View style={globalStyles.row}>
+        <CalculatorButton label="4"  />
+        <CalculatorButton label="5"  />
+        <CalculatorButton label="6"  />
+        <CalculatorButton label="-" color={colors.orange} />
+      </View>
+      <View style={globalStyles.row}>
+        <CalculatorButton label="1"  />
+        <CalculatorButton label="2"  />
+        <CalculatorButton label="3"  />
+        <CalculatorButton label="+" color={colors.orange} />
+      </View>
+      <View style={globalStyles.row}>
+        <CalculatorButton label="0"  doubleSize={true}/>
+        <CalculatorButton label="."  />
+        <CalculatorButton label="=" color={colors.orange} />
       </View>
     </View>
   );
